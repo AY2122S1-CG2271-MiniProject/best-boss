2f9a48292d5c7c7b4189929b239d57e8125347fd
izdiyadfrhn <izdiyadfarhan@gmail.com>
2021-11-09 22:57:18 +0800

    bossSensor

--- TheBoss/main.c
+++ TheBoss/main.c
@@ -134,16 +134,20 @@ int main (void) {
 	InitRGB();
 	initFrontGreenLEDGPIO();
 	initRearRedLEDGPIO();
-	InitMotor();
+	InitMotor(); //+Sensor within
 	InitAudio();
 	
 	stop();
 	offRGB();
 	offRearRedLED();
 	offFrontGreenLED();
+	while(1){
+		sensorDistance = checkDistance();
+	}
 	
   // ...
 	
+	/*
 	bossBrain = osSemaphoreNew(1,0,NULL);
 	bossAuto = osSemaphoreNew(1,0,NULL);
  
@@ -156,4 +160,5 @@ int main (void) {
 	osThreadNew(bAuto, NULL, NULL);		// Create application drive thread
 	osKernelStart();                      // Start thread execution
   for (;;) {}
+	*/
 }
